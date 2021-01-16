b63647ad716b70f4b8042d086d2201c77e5cfd3d
Lucas Miguel Calvo Coltro <lcalvo@accionpoint.com>
2021-01-15 22:09:18 -0300

    “prueba”

---
+++ index.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Transporte Scropranich</title>
+    <!--<link rel="icon" href="./images/logo_ts.jpg" sizes="1280x216">-->
+    <meta name="description" content="Transporte Scropranich - Cargas Generales - Encomiendas - Distribuciones
+Comisiones - Mudanzas -Cargas Paletizadas - Servicio Directo a Capital Federal - C.A.B.A / Vedia">
+    <meta name="keywords" content="transporte scropanich, scropanich,transporte a vedia,transporte a jun&iacute;n, servicio a capital federal, 			">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+     <link href="css/bootstrap.css" rel="stylesheet" > 
+     
+            <link rel="stylesheet" href="css/OpenSans.css">
+            <link rel="stylesheet" href="css/estilos.css">
+            <link rel="stylesheet" href="css/animate.css">
+            <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&family=Roboto:wght@300&display=swap" rel="stylesheet">
+ <script src="js/jquery.js">
+
+ </script>
+ <script src="js/fontawesome.js" crossorigin="anonymous"></script>
+ <script src="js/wow.min.js"></script>
+              <script>
+              new WOW().init();
+              </script>
+        <script src="js/bootstrap.js"></script>
+        <script type="text/javascript">
+          $('#main-carousel').carousel({
+  interval: 5000,
+  cycle: true
+}); 
+        </script>
+</head>
+<body data-spy="scroll" data-target=".navbar" data-offset="50">
+
+<nav class="navbar navbar-expand-lg fixed-top" id="navMenu">
+
+   <img src="./images/logo_ts.jpg" class="navbar-brand" id="logoPrin" href="#seccion1" alt="LogoTS">  
+
+   <a class="navbar-brand" href="#">
+  <button id="botonMenu" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <i class="fas fa-bars"></i>
+  </button>
+  </a>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link" id="textoMenu" href="#seccion1">Inicio<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" id="textoMenu" href="#seccion2">Servicios</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" id="textoMenu" href="#seccion3">Localidades</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link"  id="textoMenu" href="#seccion4">Contacto</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<div id="seccion1" class="container-fluid">
+      <div class="carousel slide" id="main-carousel" data-ride="carousel">
+      <!--<ol class="carousel-indicators">
+        <li data-target="#main-carousel" data-slide-to="0" class="active"></li>
+        <li data-target="#main-carousel" data-slide-to="1"></li>
+      </ol> /.carousel-indicators -->-->
+      
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img class="d-block img-fluid" src="./images/trucks-2.jpg" id="imgSlide1" alt="">
+          <div class="carousel-caption">
+    <h1 id="textoSlide" class="wow slideInUp" data-wow-duration="5s" data-wow-delay="0s">Transporte Scropanich</h1>
+  </div>
+        </div>
+        <div class="carousel-item">
+          <img class="d-block img-fluid" src="./images/camion1.jpg" id="imgSlide2" alt="">
+          <div class="carousel-caption">
+    <h1 id="textoSlide" class="wow slideInUp" data-wow-duration="3s" data-wow-delay="0s">Transporte Scropanich</h1>
+  </div>
+        </div>
+      </div><!-- /.carousel-inner -->
+      
+      <!--<a href="#main-carousel" class="carousel-control-prev" data-slide="prev">
+        <span class="carousel-control-prev-icon"></span>
+        <span class="sr-only" aria-hidden="true">Prev</span>
+      </a>
+      <a href="#main-carousel" class="carousel-control-next" data-slide="next">
+        <span class="carousel-control-next-icon"></span>
+        <span class="sr-only" aria-hidden="true">Next</span>
+      </a>-->
+    </div><!-- /.carousel -->
+</div>
+
+
+
+<div id="seccion2" class="container-fluid" >
+  <div class="container">
+  <div class="row">
+    <div class="col-12" id="divServicios1">
+      <i class="fas fa-truck wow bounceInLeft" data-wow-duration="3s" data-wow-delay="0s" id="iconCamion" ></i>
+</div>
+  <div class="col-4" id="divServicios">
+  <h1 id="textServicios">Cargas Generales</h1>
+</div>
+  <div class="col-4" id="divServicios">
+  <h1 id="textServicios">Encomiendas</h1>
+</div>
+ <div class="col-4" id="divServicios">
+  <h1 id="textServicios">Distribuciones</h1>
+</div>
+<div class="col-12" id="divServicios1">
+      <i class="fas fa-truck-loading wow bounceInRight" data-wow-duration="3s" data-wow-delay="0s" id="iconCamion" ></i>
+</div>
+<div class="col-4" id="divServicios">
+  <h1 id="textServicios">Comisiones</h1>
+</div>
+<div class="col-4" id="divServicios">
+  <h1 id="textServicios">Mudanzas</h1>
+</div>
+<div class="col-4" id="divServicios">
+  <h1 id="textServicios">Cargas Paletizadas</h1>
+</div>
+<div class="col-12" id="divServicios1">
+      <i class="fas fa-city wow bounceInUp" data-wow-duration="3s" data-wow-delay="0s" id="iconCamion" ></i>
+</div>
+<div class="col-12" id="divServicios">
+  <h1 id="textServicios">Servicio Directo a Capital Federal</h1>
+</div>
+</div>
+</div>
+</div>
+<div id="seccion3" class="container-fluid">
+    <div class="container">
+  <div class="row">
+  <div class="col-3 col-sm-3" id="divLocalidades">
+    <img src="./images/svg_ubicacion.svg" class="wow bounceInLeft" data-wow-duration="3s" data-wow-delay="0s" id="svgUbicacion" alt="Transportes a Vedia">
+  <h1 id="textLocalidades">Vedia</h1>
+</div>
+  <div class="col-3 col-sm-3" id="divLocalidades">
+      <img src="./images/svg_ubicacion.svg" class="wow bounceInLeft" data-wow-duration="3s" data-wow-delay="0s" id="svgUbicacion" alt="Transportes a Junín">
+  <h1 id="textLocalidades">Jun&iacute;n</h1>
+</div>
+ <div class="col-3 col-sm-3" id="divLocalidades">
+    <img src="./images/svg_ubicacion.svg" class="wow bounceInRight" data-wow-duration="3s" data-wow-delay="0s" id="svgUbicacion"  alt="Transportes a Alberdi">
+  <h1 id="textLocalidades">Alberdi</h1>
+</div>
+<div class="col-3 col-sm-3" id="divLocalidades">
+    <img src="./images/svg_ubicacion.svg" class="wow bounceInRight" data-wow-duration="3s" data-wow-delay="0s" id="svgUbicacion"  alt="Transportes a Germania">
+  <h1 id="textLocalidades">Germania</h1>
+</div>
+<div class="col-3 col-sm-3" id="divLocalidades">
+    <img src="./images/svg_ubicacion.svg" class="wow bounceInLeft" data-wow-duration="3s" data-wow-delay="0s" id="svgUbicacion" alt="Transportes a Alem">
+  <h1 id="textLocalidades">Alem</h1>
+</div>
+<div class="col-3 col-sm-3" id="divLocalidades">
+    <img src="./images/svg_ubicacion.svg"  class="wow bounceInLeft" data-wow-duration="3s" data-wow-delay="0s" id="svgUbicacion" alt="Transportes a Arenales">
+  <h1 id="textLocalidades">Arenales</h1>
+</div>
+<div class="col-3 col-sm-3" id="divLocalidades">
+    <img src="./images/svg_ubicacion.svg" class="wow bounceInRight" data-wow-duration="3s" data-wow-delay="0s"  id="svgUbicacion" alt="Transportes a Ascensión">
+  <h1 id="textLocalidades">Ascensi&oacute;n</h1>
+</div>
+<div class="col-3 col-sm-3" id="divLocalidades">
+    <img src="./images/svg_ubicacion.svg" class="wow bounceInRight" data-wow-duration="3s" data-wow-delay="0s"  id="svgUbicacion"alt="Transportes a Ferre" >
+  <h1 id="textLocalidades">Ferre</h1>
+</div>
+<div class="col-1 col-sm-1" id="divLocalidades">
+  </div>
+<div class="col-3 col-sm-3" id="divLocalidades">
+    <img src="./images/svg_ubicacion.svg" class="wow bounceInUp" data-wow-duration="3s" data-wow-delay="0s"  id="svgUbicacion" alt="Transportes a Arribeños" >
+  <h1 id="textLocalidades">Arribe&ntilde;os</h1>
+</div>
+<div class="col-3 col-sm-3" id="divLocalidades">
+    <img src="./images/svg_ubicacion.svg" class="wow bounceInUp" data-wow-duration="3s" data-wow-delay="0s"  id="svgUbicacion" alt="Transportes a Iriarte">
+  <h1 id="textLocalidades">Iriarte</h1>
+</div>
+<div class="col-3 col-sm-3" id="divLocalidades">
+    <img src="./images/svg_ubicacion.svg" class="wow bounceInUp" data-wow-duration="3s" data-wow-delay="0s"  id="svgUbicacion" alt="Transportes a San Gregorio">
+  <h1 id="textLocalidades">San Gregorio</h1>
+</div>
+<div class="col-1 col-sm-1" id="divLocalidades">
+  </div>
+</div>
+</div>
+</div>
+<div id="seccion4" class="container-fluid">
+    <div class="container">
+  <div class="row">
+    <div class="col-12" id="divUbicacion">
+
+    <h1  id="textVedia">VEDIA</h1>
+         <hr class="lineBorder">
+  </div>
+  <div class="col-12 col-sm-6" id="divServicios">
+    <h1 id="dirVedia">Direcci&oacute;n: Ranqueles 135 (6030)</h1>
+    <h1 id="telVedia">Cel.: 0236 154642711 / 4553450</h1>
+    <h1 id="telVedia">Cel.: (011) - 156002290</h1>
+    <h1 id="btnVedia"> <button type="button" class="btn" id="botonTel"><a id="linkTel" href="tel:0236 154642711">LLAMAR AHORA
+       <i class="fas fa-phone-alt" id="iconTel"></i></a></button></h1>
+          <h1 id="btnVedia"> 
+    <button type="button" class="btn" id="botonTel"><a id="linkTel" href="mailto:transportescropanich@hotmail.com">ENVIAR MAIL
+       <i class="fas fa-envelope" id="iconTel"></i></a></button>
+       </h1>
+  </div>
+  <div class="col-12 col-sm-6" id="divUbicacion">
+    <iframe id="iframeDir" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3288.4947101603952!2d-61.54968858519933!3d-34.4903404590141!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95c777b0a5376ba3%3A0x5fc7975030357a10!2sRanqueles%2C%20Vedia%2C%20Buenos%20Aires!5e0!3m2!1ses!2sar!4v1588008458416!5m2!1ses!2sar" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+  </div>
+  <div class="col-12 " id="divServicios">
+    <h1  id="textCap">CAPITAL FEDERAL</h1>
+    <hr class="lineBorder">
+  </div>
+  <div class="col-12 col-sm-6" id="divServicios">
+    <h1 id="dirCap">Direcci&oacute;n: Avda. Fernandez de la Cruz 2340 - Villa Soldati</h1>
+    <h1 id="telCap">Cel.: 0236 154642711 / 4553450</h1>
+    <h1 id="btnVedia"> 
+    <button type="button" class="btn" id="botonTel"><a id="linkTel" href="tel:0236 154642711">LLAMAR AHORA
+       <i class="fas fa-phone-alt" id="iconTel"></i></a></button>
+       </h1>
+       <h1 id="btnVedia"> 
+    <button type="button" class="btn" id="botonTel"><a id="linkTel" href="mailto:transportescropanich@hotmail.com">ENVIAR MAIL
+       <i class="fas fa-envelope" id="iconTel"></i></a></button>
+       </h1>
+  </div>
+  <div class="col-12 col-sm-6" id="divUbicacion">
+    <iframe id="iframeDir"  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3282.0247701209905!2d-58.43749098519342!3d-34.654077367670546!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bccb95e9e809dd%3A0xa9278f034536497b!2sAv.%20Gral.%20Francisco%20Fern%C3%A1ndez%20de%20la%20Cruz%202340%2C%20C1437GZP%20CABA!5e0!3m2!1ses!2sar!4v1588009040962!5m2!1ses!2sar" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+  </div>
+</div>
+</div>
+ 
+</div>
+<div class="col-12" id="footer">
+  <h4 id="footer">Todos los Derechos Reservados © 2020 | Transporte Scropanich</h1>
+  </div>
+</body>
+</html>
